4391a1d2a98055966f241e401de8498c6509f620
William Melody <wmelody@gmail.com>
2008-04-15 11:48:03 -0500

    adding aspect ratio options for MiniMagick
    Should allow for more complex resizing operations, specifically when using unconventional aspect ratios.

--- lib/technoweenie/attachment_fu/processors/mini_magick_processor.rb
+++ lib/technoweenie/attachment_fu/processors/mini_magick_processor.rb
@@ -36,22 +36,43 @@ module Technoweenie # :nodoc:
         end
 
         # Performs the actual resizing operation for a thumbnail
+        # http://pastie.caboo.se/129911
+        # http://blog.iandrysdale.com/2007/05/22/cropped-thumbnails-in-attachment_fu-using-mini_magick/
         def resize_image(img, size)
           size = size.first if size.is_a?(Array) && size.length == 1
-          img.combine_options do |commands|
-            commands.strip unless attachment_options[:keep_profile]
-            if size.is_a?(Fixnum) || (size.is_a?(Array) && size.first.is_a?(Fixnum))
-              if size.is_a?(Fixnum)
-                size = [size, size]
-                commands.resize(size.join('x'))
-              else
-                commands.resize(size.join('x') + '!')
-              end
+          if size.is_a?(Fixnum) || (size.is_a?(Array) && size.first.is_a?(Fixnum))
+            if size.is_a?(Fixnum)
+              size = [size, size]
+              img.resize(size.join('x'))
             else
-              commands.resize(size.to_s)
+              img.resize(size.join('x') + '!')
             end
+          else
+            n_size = [img[:width], img[:height]] / size.to_s
+            if size.ends_with? "!"
+              aspect = n_size[0].to_f / n_size[1].to_f
+              ih, iw = img[:height], img[:width]
+              if aspect > 1 and iw > ih
+                w, h = (ih / aspect), (iw * aspect)
+                w = [iw, w].min.to_i
+                h = [ih, h].min.to_i
+                w, h = h, w
+              else
+                w, h = (ih * aspect), (iw / aspect)
+                w = [iw, w].min.to_i
+                h = [ih, h].min.to_i
+              end
+              
+              offset = 0.5
+              img.gravity 'center'
+              img.crop("#{w}x#{h}+#{offset * (iw-w)}+#{offset * (ih-h)}\!")
+              
+              img.resize(size.to_s)
+            else
+              img.resize(size.to_s)
+            end
+            self.temp_path = img
           end
-          self.temp_path = img
         end
       end
     end
